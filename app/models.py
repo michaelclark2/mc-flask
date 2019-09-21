e3cb17e6cb2a154bab21bcf82b1b0ff50f8391fc
michael <michael.clark1992@gmail.com>
2019-09-20 22:54:14 -0500

    added admin, with auth, and changed schemas

--- app/models.py
+++ app/models.py
@@ -1,24 +1,34 @@
 from app import db
+from flask_login import UserMixin
 
-association_table = db.Table('association', db.metadata,
-  db.Column('project_id', db.Integer, db.ForeignKey('project.id')),
-  db.Column('tech_id', db.Integer, db.ForeignKey('tech.id')))
+association_table = db.Table('project_techs', db.metadata,
+  db.Column('project_id', db.Integer, db.ForeignKey('projects.id')),
+  db.Column('tech_id', db.Integer, db.ForeignKey('techs.id')))
 
 class Project(db.Model):
-  __tablename__ = 'project'
+  __tablename__ = 'projects'
   id = db.Column(db.Integer, primary_key=True)
   title = db.Column(db.String(200), index=True, unique=True)
   desc = db.Column(db.String(2000))
+  github = db.Column(db.String(200))
+  url = db.Column(db.String(200))
+  thumbnail = db.Column(db.String(500))
   techs = db.relationship("Tech", secondary=association_table)
 
   def __repr__(self):
     return '<Project {}>'.format(self.title)
 
+  def __str__(self):
+    return self.title
+
 class Tech(db.Model):
-  __tablename__ = 'tech'
+  __tablename__ = 'techs'
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(100), index=True, unique=True)
   icon = db.Column(db.String(500), index=True, unique=True)
 
   def __repr__(self):
     return '<Tech {}>'.format(self.name)
+
+  def __str__(self):
+    return self.name
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,24 @@
-from app import app
+from app import app, admin, db, models, login
+from flask_admin.contrib.sqla import ModelView
+from flask import redirect, session, request
+
+class PrivateView(ModelView):
+  def is_accessible(self):
+    return session.get('authed')
+
+admin.add_view(PrivateView(models.Project, db.session))
+admin.add_view(PrivateView(models.Tech, db.session))
+
+@app.route('/admin/login', methods=['POST'])
+def login_admin():
+  if request.form['password'] == app.config['ADMIN_PASSWORD']:
+    session['authed'] = True
+    return redirect('/admin')
+  return 'ayelmao'
+
 
 @app.route('/')
 def index():
-  return "<h1>hello world</h1>"
+  return '<h1>hello world</h1>'
+
+
