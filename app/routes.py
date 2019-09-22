535a7c3915f5d0bdebf78be9f954e89d70d567cb
michael <michael.clark1992@gmail.com>
2019-09-21 23:34:25 -0500

    gets project by id, wow so easy

--- app/routes.py
+++ app/routes.py
@@ -26,3 +26,8 @@ def get_all_projects():
   projects = models.Project.query.all()
   return jsonify(projects)
 
+@app.route('/p/<int:project_id>')
+def get_project_by_id(project_id):
+  project = models.Project.query.get(project_id)
+  return jsonify(project)
+
