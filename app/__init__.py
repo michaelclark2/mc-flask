ddc4020413bdbf08d9cf98239763966a00857043
michael <michael.clark1992@gmail.com>
2019-09-21 23:21:28 -0500

    added cors

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,14 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_admin import Admin
-from flask_login import LoginManager
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(Config)
+CORS(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 admin = Admin(app)
-login = LoginManager(app)
 
 
 from app import routes, models
